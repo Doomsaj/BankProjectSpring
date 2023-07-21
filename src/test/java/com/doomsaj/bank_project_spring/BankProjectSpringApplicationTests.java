39f7c31f29ca78a5438fd133c4d5b1c25a46e35a
mohammad sajjad <m.s.e.r1382@gmail.com>
2023-07-21 10:38:37 +0330

    initial commit

---
+++ src/test/java/com/doomsaj/bank_project_spring/BankProjectSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doomsaj.bank_project_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankProjectSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
